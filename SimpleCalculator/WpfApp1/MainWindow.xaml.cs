88f33a946a428da66beddd28cdcebb4d6b633760
emacslisp <java.gdb@gmail.com>
2017-06-24 10:25:01 +1000

    finish example with history

--- SimpleCalculator/WpfApp1/MainWindow.xaml.cs
+++ SimpleCalculator/WpfApp1/MainWindow.xaml.cs
@@ -27,27 +27,94 @@ namespace WpfApp1
 
         private void Del_Click(object sender, RoutedEventArgs e)
         {
-
+            tb.Text = String.Empty;
         }
 
         private void Result_click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                result();
+            }
+            catch (Exception exc)
+            {
+                tb.Text = "Error!";
+            }
+        }
 
+        private void result()
+        {
+            String op;
+            int iOp = 0;
+            if (tb.Text.Contains("+"))
+            {
+                iOp = tb.Text.IndexOf("+");
+            }
+            else if (tb.Text.Contains("-"))
+            {
+                iOp = tb.Text.IndexOf("-");
+            }
+            else if (tb.Text.Contains("*"))
+            {
+                iOp = tb.Text.IndexOf("*");
+            }
+            else if (tb.Text.Contains("/"))
+            {
+                iOp = tb.Text.IndexOf("/");
+            }
+            else
+            {
+                //error
+            }
+
+            op = tb.Text.Substring(iOp, 1);
+            double op1 = Convert.ToDouble(tb.Text.Substring(0, iOp));
+            double op2 = Convert.ToDouble(tb.Text.Substring(iOp + 1, tb.Text.Length - iOp - 1));
+
+            if (op == "+")
+            {
+                tb.Text += "=" + (op1 + op2);
+            }
+            else if (op == "-")
+            {
+                tb.Text += "=" + (op1 - op2);
+            }
+            else if (op == "*")
+            {
+                tb.Text += "=" + (op1 * op2);
+            }
+            else
+            {
+                tb.Text += "=" + (op1 / op2);
+            }
+
+            tbHistory.AppendText(tb.Text + Environment.NewLine);
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
+            /*
+             TODO: 1. check whether operation is existing or not.
+             */
+
+            Button b = (Button)sender;
+            tb.Text += b.Content.ToString();
 
         }
 
         private void R_Click(object sender, RoutedEventArgs e)
         {
-
+            if (tb.Text.Length > 0)
+            {
+                tb.Text = tb.Text.Substring(0, tb.Text.Length - 1);
+            }
         }
 
         private void Off_Click_1(object sender, RoutedEventArgs e)
         {
+            this.Close();
 
+            // Application.Current.Shutdown();
         }
     }
 }
