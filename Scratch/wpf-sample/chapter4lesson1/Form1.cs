c4a0abbc450f10ad365943627f55dd2aedc7ba14
emacslisp <java.gdb@gmail.com>
2018-01-02 15:03:34 +1100

    finish winforms chapter 4

--- Scratch/wpf-sample/chapter4lesson1/Form1.cs
+++ Scratch/wpf-sample/chapter4lesson1/Form1.cs
@@ -12,9 +12,44 @@ namespace chapter4lesson1
 {
     public partial class Form1 : Form
     {
+        public bool isFlowBreak = false;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            isFlowBreak = !isFlowBreak;
+            flowLayoutPanel1.SetFlowBreak(button3, isFlowBreak);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Button aButton = new Button();
+            tableLayoutPanel1.Controls.Add(aButton, 1, 1);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if(splitContainer1.FixedPanel == FixedPanel.Panel1)
+            {
+                splitContainer1.FixedPanel = FixedPanel.None;
+            }
+            else
+            {
+                splitContainer1.FixedPanel = FixedPanel.Panel1;
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            splitContainer1.IsSplitterFixed = !(splitContainer1.IsSplitterFixed);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            splitContainer1.Panel1Collapsed = !(splitContainer1.Panel1Collapsed);
         }
     }
 }
